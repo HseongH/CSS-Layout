f585d4c6e17d73c1087024edc5f92d67b9f7c721
hongseonghun-web <hh4518@gmail.com>
2021-04-29 23:38:33 +0900

    이미지 미리보기 기능 수정

--- js/image-preview.js
+++ js/image-preview.js
@@ -1,12 +1,18 @@
 layout.createObj('preview');
 
 layout.preview.imagePreview = function() {
+    if (layout.preview.showImg.querySelector('img')) {
+        const image = layout.preview.showImg.querySelector('img');
+        layout.preview.showImg.removeChild(image);
+    }
+
     if (!this.value) return;
 
     const enterImg = document.createElement('img');
     enterImg.src = this.value;
     enterImg.alt = 'image';
 
+    layout.preview.showImg.classList.add('preview');
     layout.preview.showImg.insertBefore(enterImg, layout.preview.showImg.firstChild);
 }
 
@@ -15,3 +21,4 @@ layout.preview.showImg = document.querySelector('.show-image');
 layout.preview.warning = document.querySelector('.warning');
 
 layout.preview.imgURL.addEventListener('change', layout.preview.imagePreview);
+layout.preview.imgURL.addEventListener('click', layout.preview.imgURL.select);
