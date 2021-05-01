7e7fbf07e24e7709e7bc9baf99e83ef3fd96da6a
unknown <hh4518@gmail.com>
2021-05-01 19:16:34 +0900

    add image validation

--- js/enter-image.js
+++ js/enter-image.js
@@ -4,20 +4,18 @@ layout.enterImg.enterImg = function() {
     const imgBox = layout.selectedBox;
     layout.preview.removeImage(imgBox);
 
-    if (!layout.preview.imgURL.value) {
-        imgBox.classList.remove('image-box');
-    } else {
-        const enterImg = document.createElement('img');
-        enterImg.src = layout.preview.imgURL.value;
-        enterImg.alt = 'image';
+    const enterImg = document.createElement('img');
+    enterImg.src = layout.preview.imgURL.value;
+    enterImg.alt = 'image';
 
-        imgBox.appendChild(enterImg);
-        imgBox.classList.add('image-box');
-    }
+    imgBox.appendChild(enterImg);
+    imgBox.classList.add('image-box');
 
     layout.displayed.hideElement(layout.displayed.enterImg);
     layout.displayed.enterImgIsHide();
 }
 
 layout.enterImg.enterImgBtn = document.querySelector('.btn--enter-image');
-layout.enterImg.enterImgBtn.addEventListener('click', layout.enterImg.enterImg);
+layout.enterImg.enterImgBtn.addEventListener('click', function() {
+    layout.preview.imageValidation(layout.preview.imgURL.value, this);
+});
--- js/image-preview.js
+++ js/image-preview.js
@@ -1,5 +1,40 @@
 layout.createObj('preview');
 
+layout.preview.imageValidation = function(url, elem) {
+    if (!url) {
+        layout.preview.removeImage(layout.selectedBox);
+        layout.selectedBox.classList.remove('image-box');
+        layout.displayed.hideElement(layout.displayed.enterImg);
+        layout.displayed.enterImgIsHide();
+
+        return;
+    }
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url, true);
+
+    xhr.send();
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+
+        if (xhr.status === 200) {
+            if (elem === layout.preview.imgURL) {
+                layout.preview.imagePreview(url);
+                return;
+            }
+
+            layout.enterImg.enterImg();
+        } else {
+            layout.displayed.showElement(layout.preview.warning);
+            layout.preview.warning.querySelector('.warning__text').innerText = '이미지 주소가 유효하지 않습니다.';
+
+            return false;
+        }
+    }
+}
+
 layout.preview.warningDisplay = function(image) {
     if (!image) return;
 
@@ -9,9 +44,11 @@ layout.preview.warningDisplay = function(image) {
 
         if (naturalSize < boxSize) {
             layout.displayed.showElement(layout.preview.warning);
-        } else {
-            layout.displayed.hideElement(layout.preview.warning);
-        }
+            layout.preview.warning.querySelector('.warning__text').innerText = '이미지 크기가 작아 깨질 수 있습니다.';
+            return;
+        } 
+        
+        layout.displayed.hideElement(layout.preview.warning);
     }
 }
 
@@ -22,27 +59,24 @@ layout.preview.removeImage = function(imgCon=layout.preview.showImg) {
     imgCon.removeChild(image);
 }
 
-layout.preview.imagePreview = function() {
+layout.preview.imagePreview = function(url) {
     layout.preview.removeImage();
 
-    if (!this.value) {
-        layout.preview.showImg.classList.remove('preview');
-        return;
-    }
-
-    const enterImg = document.createElement('img');
-    enterImg.src = this.value;
+    const enterImg = new Image;
+    enterImg.setAttribute('src', url);
     enterImg.alt = 'image';
-
-    layout.preview.showImg.classList.add('preview');
+    
+    layout.preview.warningDisplay(enterImg);
     layout.preview.showImg.insertBefore(enterImg, layout.preview.showImg.firstChild);
 
-    layout.preview.warningDisplay(enterImg);
+    layout.preview.showImg.classList.add('preview');
 }
 
 layout.preview.imgURL = document.getElementById('input--image-url');
 layout.preview.showImg = document.querySelector('.show-image');
 layout.preview.warning = document.querySelector('.warning');
 
-layout.preview.imgURL.addEventListener('change', layout.preview.imagePreview);
+layout.preview.imgURL.addEventListener('change', function() {
+    layout.preview.imageValidation(this.value, this);
+});
 layout.preview.imgURL.addEventListener('click', layout.preview.imgURL.select);
